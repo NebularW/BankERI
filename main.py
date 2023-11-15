160eedd7ae8866c1b4e10bf8f28410d7acb0037c
Nebular <nebularw@outlook.com>
2023-11-15 14:31:48 +0800

    doc to docx

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 
 from flask import Flask, request
+from gensim import corpora,models,similarities
 
-from rule import get_external_rule, get_original_text
+from rule import get_external_rule, get_original_text, get_internal_rules
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def page():
     return 'Hello, World!'
 
 
@@ -34,12 +35,22 @@ def external_text():
 @app.route('/cal')
 def calculate():
     name = request.args.get('name')
-    external_rule = get_external_rule(external_path, name)
-    return external_rule.word_count
+    external_rule = get_external_rule(external_path, name).words
+    external_rule_vec = dictionary.doc2bow(external_rule)
+    sim = index[tfidf[external_rule_vec]]
+    result = sorted(enumerate(sim), key=lambda item: -item[1])
+    
+    print(result)
+    return result
 
 
 if __name__ == '__main__':
     internal_path = './data/internal'
     external_path = './data/external'
-    # internal_rules = get_internal_rules(internal_path)
+    internal_rules = get_internal_rules(internal_path)
+    internal_rule_list = [rule.words for rule in internal_rules]
+    dictionary = corpora.Dictionary(internal_rule_list)
+    corpus = [dictionary.doc2bow(doc) for doc in internal_rule_list]
+    tfidf = models.TfidfModel(corpus)
+    index = similarities.SparseMatrixSimilarity(tfidf[corpus], num_features=len(dictionary.keys()))
     app.run(debug=True)
