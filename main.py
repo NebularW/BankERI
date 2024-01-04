42f916a41c90c72d3f9f3ca9e62ac87109e54fa1
Nebular <nebularw@outlook.com>
2024-01-05 00:43:49 +0800

    commit

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def cal():
     return result_str
 
 
-@app.route('/upload', method=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
     text = request.args.get('text')
     external_rule = Rule(0, '', '', '', text).words
